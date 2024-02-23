411b6ab31b5f2a5f5df93fc1c98c4e036d1159ae
yxy <summerhh54@gmail.com>
2024-02-23 18:16:52 +0000

    delete useless code

--- src/layer/riscv/convolutiondepthwise_3x3_pack8_int8.h
+++ src/layer/riscv/convolutiondepthwise_3x3_pack8_int8.h
@@ -20,7 +20,7 @@ static void convdw3x3s1_pack8_int8_rvv(const Mat& bottom_blob, Mat& top_blob, co
     int outw = top_blob.w;
     int outh = top_blob.h;
 
-    int vl = csrr_vlenb() / 2;
+    const int vl = csrr_vlenb() / 2;
 
     const int group = bottom_blob.c;
 
@@ -41,7 +41,6 @@ static void convdw3x3s1_pack8_int8_rvv(const Mat& bottom_blob, Mat& top_blob, co
         const signed char* r2 = img0.row<const signed char>(2);
         const signed char* r3 = img0.row<const signed char>(3);
 
-        vl = 8;
         vint8m1_t _k00 = vle8_v_i8m1(k0, vl);
         vint8m1_t _k01 = vle8_v_i8m1(k0 + 8, vl);
         vint8m1_t _k02 = vle8_v_i8m1(k0 + 16, vl);
@@ -51,16 +50,6 @@ static void convdw3x3s1_pack8_int8_rvv(const Mat& bottom_blob, Mat& top_blob, co
         vint8m1_t _k20 = vle8_v_i8m1(k0 + 48, vl);
         vint8m1_t _k21 = vle8_v_i8m1(k0 + 56, vl);
         vint8m1_t _k22 = vle8_v_i8m1(k0 + 64, vl);
-
-        // int8x8_t _k00 = vld1_s8(k0);
-        // int8x8_t _k01 = vld1_s8(k0 + 8);
-        // int8x8_t _k02 = vld1_s8(k0 + 16);
-        // int8x8_t _k10 = vld1_s8(k0 + 24);
-        // int8x8_t _k11 = vld1_s8(k0 + 32);
-        // int8x8_t _k12 = vld1_s8(k0 + 40);
-        // int8x8_t _k20 = vld1_s8(k0 + 48);
-        // int8x8_t _k21 = vld1_s8(k0 + 56);
-        // int8x8_t _k22 = vld1_s8(k0 + 64);
 
         int i = 0;
         for (; i < outh; i++)
@@ -78,38 +67,20 @@ static void convdw3x3s1_pack8_int8_rvv(const Mat& bottom_blob, Mat& top_blob, co
                 vint8m1_t _r21 = vle8_v_i8m1(r2 + 8, vl);
                 vint8m1_t _r22 = vle8_v_i8m1(r2 + 16, vl);
 
-                // int8x8_t _r00 = vld1_s8(r0);
-                // int8x8_t _r01 = vld1_s8(r0 + 8);
-                // int8x8_t _r02 = vld1_s8(r0 + 16);
-                // int8x8_t _r10 = vld1_s8(r1);
-                // int8x8_t _r11 = vld1_s8(r1 + 8);
-                // int8x8_t _r12 = vld1_s8(r1 + 16);
-                // int8x8_t _r20 = vld1_s8(r2);
-                // int8x8_t _r21 = vld1_s8(r2 + 8);
-                // int8x8_t _r22 = vld1_s8(r2 + 16);
 
                 vint16m2_t _s0 = vwmul_vv_i16m2(_r00, _k00, vl);
                 vint16m2_t _s1 = vwmul_vv_i16m2(_r01, _k01, vl);
                 vint16m2_t _s2 = vwmul_vv_i16m2(_r02, _k02, vl);
                 vint16m2_t _s3 = vwmul_vv_i16m2(_r10, _k10, vl);
 
-                // int16x8_t _s0 = vmull_s8(_r00, _k00);
-                // int16x8_t _s1 = vmull_s8(_r01, _k01);
-                // int16x8_t _s2 = vmull_s8(_r02, _k02);
-                // int16x8_t _s3 = vmull_s8(_r10, _k10);
 
                 _s0 = vwmacc_vv_i16m2(_s0, _r11, _k11, vl);
                 _s1 = vwmacc_vv_i16m2(_s1, _r12, _k12, vl);
                 _s2 = vwmacc_vv_i16m2(_s2, _r20, _k20, vl);
                 _s3 = vwmacc_vv_i16m2(_s3, _r21, _k21, vl);
 
-                // _s0 = vmlal_s8(_s0, _r11, _k11);
-                // _s1 = vmlal_s8(_s1, _r12, _k12);
-                // _s2 = vmlal_s8(_s2, _r20, _k20);
-                // _s3 = vmlal_s8(_s3, _r21, _k21);
 
                 vint16m2_t _s4 = vwmul_vv_i16m2(_r22, _k22, vl);
-                // int16x8_t _s4 = vmull_s8(_r22, _k22);
 
                 vint16m1_t _s0_m1 = vget_v_i16m2_i16m1(_s0, 0);
                 vint16m1_t _s1_m1 = vget_v_i16m2_i16m1(_s1, 0);
@@ -122,18 +93,8 @@ static void convdw3x3s1_pack8_int8_rvv(const Mat& bottom_blob, Mat& top_blob, co
                 _sum = vwadd_wv_i32m2(_sum, _s3_m1, vl);
                 _sum = vwadd_wv_i32m2(_sum, _s4_m1, vl);
 
-                // int32x4_t _sum0 = vaddl_s16(vget_low_s16(_s0), vget_low_s16(_s1));
-                // int32x4_t _sum1 = vaddl_s16(vget_high_s16(_s0), vget_high_s16(_s1));
-                // int32x4_t _sum2 = vaddl_s16(vget_low_s16(_s2), vget_low_s16(_s3));
-                // int32x4_t _sum3 = vaddl_s16(vget_high_s16(_s2), vget_high_s16(_s3));
-                // _sum0 = vaddw_s16(_sum0, vget_low_s16(_s4));
-                // _sum1 = vaddw_s16(_sum1, vget_high_s16(_s4));
-                // _sum0 = vaddq_s32(_sum0, _sum2);
-                // _sum1 = vaddq_s32(_sum1, _sum3);
 
                 vse32_v_i32m2(outptr0, _sum, vl);
-                // vst1q_s32(outptr0, _sum0);
-                // vst1q_s32(outptr0 + 4, _sum1);
                 r0 += 8;
                 r1 += 8;
                 r2 += 8;
@@ -153,7 +114,7 @@ static void convdw3x3s2_pack8_int8_rvv(const Mat& bottom_blob, Mat& top_blob, co
 
     int outw = top_blob.w;
     int outh = top_blob.h;
-    int vl = csrr_vlenb() / 2;
+    const int vl = csrr_vlenb() / 2;
 
     const int group = bottom_blob.c;
 
@@ -184,16 +145,6 @@ static void convdw3x3s2_pack8_int8_rvv(const Mat& bottom_blob, Mat& top_blob, co
         vint8m1_t _k21 = vle8_v_i8m1(k0 + 56, vl);
         vint8m1_t _k22 = vle8_v_i8m1(k0 + 64, vl);
 
-        // int8x8_t _k00 = vld1_s8(k0);
-        // int8x8_t _k01 = vld1_s8(k0 + 8);
-        // int8x8_t _k02 = vld1_s8(k0 + 16);
-        // int8x8_t _k10 = vld1_s8(k0 + 24);
-        // int8x8_t _k11 = vld1_s8(k0 + 32);
-        // int8x8_t _k12 = vld1_s8(k0 + 40);
-        // int8x8_t _k20 = vld1_s8(k0 + 48);
-        // int8x8_t _k21 = vld1_s8(k0 + 56);
-        // int8x8_t _k22 = vld1_s8(k0 + 64);
-
         int i = 0;
         for (; i < outh; i++)
         {
@@ -210,15 +161,6 @@ static void convdw3x3s2_pack8_int8_rvv(const Mat& bottom_blob, Mat& top_blob, co
                 vint8m1_t _r21 = vle8_v_i8m1(r2 + 8, vl);
                 vint8m1_t _r22 = vle8_v_i8m1(r2 + 16, vl);
 
-                // int8x8_t _r00 = vld1_s8(r0);
-                // int8x8_t _r01 = vld1_s8(r0 + 8);
-                // int8x8_t _r02 = vld1_s8(r0 + 16);
-                // int8x8_t _r10 = vld1_s8(r1);
-                // int8x8_t _r11 = vld1_s8(r1 + 8);
-                // int8x8_t _r12 = vld1_s8(r1 + 16);
-                // int8x8_t _r20 = vld1_s8(r2);
-                // int8x8_t _r21 = vld1_s8(r2 + 8);
-                // int8x8_t _r22 = vld1_s8(r2 + 16);
 
                 vint16m2_t _s0 = vwmul_vv_i16m2(_r00, _k00, vl);
                 vint16m2_t _s1 = vwmul_vv_i16m2(_r01, _k01, vl);
@@ -231,16 +173,6 @@ static void convdw3x3s2_pack8_int8_rvv(const Mat& bottom_blob, Mat& top_blob, co
                 _s3 = vwmacc_vv_i16m2(_s3, _r21, _k21, vl);
 
                 vint16m2_t _s4 = vwmul_vv_i16m2(_r22, _k22, vl);
-
-                // int16x8_t _s0 = vmull_s8(_r00, _k00);
-                // int16x8_t _s1 = vmull_s8(_r01, _k01);
-                // int16x8_t _s2 = vmull_s8(_r02, _k02);
-                // int16x8_t _s3 = vmull_s8(_r10, _k10);
-                // _s0 = vmlal_s8(_s0, _r11, _k11);
-                // _s1 = vmlal_s8(_s1, _r12, _k12);
-                // _s2 = vmlal_s8(_s2, _r20, _k20);
-                // _s3 = vmlal_s8(_s3, _r21, _k21);
-                // int16x8_t _s4 = vmull_s8(_r22, _k22);
 
                 vint16m1_t _s0_m1 = vget_v_i16m2_i16m1(_s0, 0);
                 vint16m1_t _s1_m1 = vget_v_i16m2_i16m1(_s1, 0);
@@ -255,17 +187,6 @@ static void convdw3x3s2_pack8_int8_rvv(const Mat& bottom_blob, Mat& top_blob, co
 
                 vse32_v_i32m2(outptr0, _sum, vl);
 
-                // int32x4_t _sum0 = vaddl_s16(vget_low_s16(_s0), vget_low_s16(_s1));
-                // int32x4_t _sum1 = vaddl_s16(vget_high_s16(_s0), vget_high_s16(_s1));
-                // int32x4_t _sum2 = vaddl_s16(vget_low_s16(_s2), vget_low_s16(_s3));
-                // int32x4_t _sum3 = vaddl_s16(vget_high_s16(_s2), vget_high_s16(_s3));
-                // _sum0 = vaddw_s16(_sum0, vget_low_s16(_s4));
-                // _sum1 = vaddw_s16(_sum1, vget_high_s16(_s4));
-                // _sum0 = vaddq_s32(_sum0, _sum2);
-                // _sum1 = vaddq_s32(_sum1, _sum3);
-
-                // vst1q_s32(outptr0, _sum0);
-                // vst1q_s32(outptr0 + 4, _sum1);
 
                 r0 += 16;
                 r1 += 16;
